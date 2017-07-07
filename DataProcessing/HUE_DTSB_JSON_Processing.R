4c5d400ee1d5ac12b7357cd140df6d4d093319dd
Matthew Sisk <MSisk1@nd.edu>
2017-07-07 11:01:45 -0400

    redid the districts map with all of the districts

--- DataProcessing/HUE_DTSB_JSON_Processing.R
+++ DataProcessing/HUE_DTSB_JSON_Processing.R
@@ -3,15 +3,29 @@ rm(list=ls(all=TRUE)) # clear memory
 
 packages<- c("maptools","rgdal","leaflet","htmlwidgets") # list the packages that you'll need
 lapply(packages, require, character.only=T) # load the packages, if they don't load you might need to install them first
-#setwd("E:\\GIT_Checkouts\\BuildingSouthBend\\DataProcessing\\")
+setwd("N:/www/BuildingSouthBend/DataProcessing")
 out.dir.name <- "WebData"
 data.dir.name <- "SpatialData"
 district.json.name <- file.path("../LeafletModules/WebData","Districts.js")
 out.path <- file.path(getwd(),out.dir.name)
 
 
+property.download <- FALSE
 
-active.sections <- c("West Washington","Downtown","Chapin Park","West North Shore")
+
+active.sections <- c("West Washington","Downtown","Chapin Park","West North Shore", "River Bend","Riverside Drive","East Wayne Street",
+                     "Edgewater Place","Lincolnway East","North St. Joseph Street","Taylor's Field")
+if(property.download){
+        download.file("http://buildingsouthbend.nd.edu/properties.csv", destfile = "properties.csv")
+        buildings.table <- read.csv("properties.csv",stringsAsFactors = F)
+        coordinates(buildings.table)=~Longitude+Latitude
+        proj4string(buildings.table) <- CRS(latlong)
+        writeOGR(buildings.table, dsn="." ,layer="All_SB_Buildings_20170706",driver="ESRI Shapefile",overwrite_layer = T)
+        
+}
+
+
+
 
 #Loading and subsetting historic district data
 historic.districts <- readOGR(dsn = "SpatialData", layer = "Historic_Districts_LL")
@@ -27,53 +41,40 @@ historic.districts@data$used <- 0
 historic.districts@data$used[historic.districts@data$District_N %in% active.sections] <- 1
 
 
+
+writeOGR(historic.districts, "temp", layer="historic.districts", driver="GeoJSON")
+file.copy("temp", district.json.name, overwrite = T) #for some reason I can't get writeOGR to write to a subdirectory, so this just copys it.
+file.remove("temp") #and deletes the temp file
+text.json <- readChar(district.json.name, file.info(district.json.name)$size)
+text.json <- paste("var districts = ",text.json,";", sep="") 
+write(text.json,district.json.name)
+
+
 #Loading and subsetting building data
-# buildings.table <- read.csv("Historic-District-Structures_ALL.csv")
-# coordinates(buildings.table)=~Long+Lat
-# proj4string(buildings.table) <- CRS("+init=epsg:3857")
-# sub.buildings <- buildings.table[sub.districts, ]
+buildings.table <- read.csv("Historic-District-Structures_ALL.csv")
+coordinates(buildings.table)=~Long+Lat
+proj4string(buildings.table) <- CRS("+init=epsg:3857")
+sub.buildings <- buildings.table[sub.districts, ]
 # 
 # 
 
 
 #writeOGR(sub.districts, dsn="sub.districts.kml", layer= "sub.districts", driver="KML", dataset_options=c("NameField=name"))
 
+#This is for updating the buildings
 
-writeOGR(historic.districts, "temp", layer="historic.districts", driver="GeoJSON")
-file.copy("temp", district.json.name, overwrite = T)
-file.remove("temp")
-text.json <- readChar(district.json.name, file.info(district.json.name)$size)
-text.json <- paste("var districts = ",text.json,";", sep="") 
-write(text.json,district.json.name)
-
-
+download.file("http://buildingsouthbend.nd.edu/properties.csv", destfile = "properties.csv")
+buildings.table <- read.csv("properties.csv",stringsAsFactors = F)
+coordinates(buildings.table)=~Longitude+Latitude
+proj4string(buildings.table) <- CRS(latlong)
+writeOGR(buildings.table, dsn="." ,layer="All_SB_Buildings_20170706",driver="ESRI Shapefile",overwrite_layer = T)
 
 
-if(F){
+
+
+
+if(F){ #This is a bunch of testing garbage
         again.json <- readChar( file.path("../LeafletModules/WebData","again.js"), file.info( file.path("../LeafletModules/WebData","again.js"))$size)
         District.json <- readChar(district.json.name, file.info(district.json.name)$size)
         identical(again.json,District.json)        
 }
-
-# 
-# #Creating the leaflet instance
-# 
-# m <- leaflet() %>%
-#         addProviderTiles("Esri.WorldTopoMap", group = "ESRI World Topo") %>%
-# #         addTiles(group = "OpenStreetMap") %>%  # Add default OpenStreetMap map tiles
-# # #         addMarkers(data=sub.buildings, group = "Locations") %>%
-# #         addCircleMarkers(data=sub.buildings,
-# #                          radius = 3,
-# #                          color = "green",
-# #                          stroke = FALSE, fillOpacity = 1, group = "Locations") %>%
-#         addPolygons(data=sub.districts, group = "Historic Disctricts")
-# # %>%
-# #         addLayersControl(
-# #                 baseGroups = c("ESRI World Topo","OpenStreetMap","Letarouilly"),
-# #                 overlayGroups = c("Locations", "Historic Disctricts"),
-# #                 options = layersControlOptions(collapsed = FALSE)
-# #         )
-# m
-# 
-
-# saveWidget(widget = m, file=paste("","your_map.html",sep=""), selfcontained = FALSE)
